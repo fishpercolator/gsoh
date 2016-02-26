ded1109dc02e70ab1bf57cd23f1cb057600a30b0
Rich Daley <rich@fishpercolator.co.uk>
2016-02-26 15:55:14 +0000

    Add some spec for the Area#features

--- spec/models/area_spec.rb
+++ spec/models/area_spec.rb
@@ -1,8 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Area, type: :model do
-  describe '.features' do
-    it 'finds features inside the area'
-    it 'does not find features outside the area'
+  
+  subject do
+    create :area, geography: {n: 53.8177, w: -1.5252, s: 53.8057, e: -1.5051}
+  end
+  
+  describe '#features' do
+    let!(:feature_inside_area) { create :feature, lat: 53.8090, lng: -1.5103 }
+    let!(:feature_outside_lat) { create :feature, lat: 53.9922, lng: -1.5103 }
+    let!(:feature_outside_lng) { create :feature, lat: 53.8090, lng: -1.5010 }
+    
+    it 'finds features inside the area' do
+      expect(subject.features).to include(feature_inside_area)
+    end
+    
+    it 'does not find features outside the area' do
+      expect(subject.features).not_to include(feature_outside_lat)
+      expect(subject.features).not_to include(feature_outside_lng)
+    end
   end
 end
