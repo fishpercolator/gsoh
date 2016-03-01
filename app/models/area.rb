57d0d94590fca7f22a42d86d08391884f9b8a338
Rich Daley <rich@fishpercolator.co.uk>
2016-03-01 16:08:45 +0000

    Add Area#contains?

--- app/models/area.rb
+++ app/models/area.rb
@@ -5,4 +5,11 @@ class Area < ActiveRecord::Base
     Feature.where(lat: geography[:s]..geography[:n], lng: geography[:w]..geography[:e])
   end
   
+  def contains?(ftype, subtype: nil)
+    conditions = {ftype: ftype}.tap do |c|
+      c[:subtype] = subtype if subtype
+    end
+    features.where(conditions).any?
+  end
+  
 end
--- spec/models/area_spec.rb
+++ spec/models/area_spec.rb
@@ -20,4 +20,25 @@ RSpec.describe Area, type: :model do
       expect(subject.features).not_to include(feature_outside_lng)
     end
   end
+  
+  describe '#contains?' do
+    let!(:pharmacy_inside) { create :feature, ftype: 'pharmacy', lat: 53.8090, lng: -1.5103 }
+    let!(:church_inside)   { create :feature, ftype: 'place_of_worship', subtype: 'christian', lat: 53.8122, lng: -1.5111 }
+    let!(:mosque_outside)  { create :feature, ftype: 'place_of_worship', subtype: 'muslim', lat: 53.9922, lng: -1.5103 }
+    let!(:school_outside)  { create :feature, ftype: 'school', lat: 53.8090, lng: -1.5010 }
+    
+    it 'contains pharmacy' do
+      expect(subject.contains?('pharmacy')).to be_truthy
+    end
+    it 'does not contain school' do
+      expect(subject.contains?('school')).not_to be_truthy
+    end
+    it 'contains church' do
+      expect(subject.contains?('place_of_worship', subtype: 'christian')).to be_truthy
+    end
+    it 'does not contain mosque' do
+      expect(subject.contains?('place_of_worship', subtype: 'mosque')).not_to be_truthy
+    end
+  end
+  
 end
