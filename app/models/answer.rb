c59b291d6f95ca924b50dbf22f5e5c397246fdf8
Rich Daley <rich@fishpercolator.co.uk>
2016-03-02 10:18:03 +0000

    Subtype deletion instead of validation. First pass at score_area (needs specs)

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,9 +2,16 @@ class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
   validates_presence_of :type, :user, :question, :answer
-  validates_presence_of :subtype, if:     -> { question.ask_subtype }
-  validates_absence_of  :subtype, unless: -> { question.ask_subtype }
   validate :question_matches
+  
+  # Remove subtype from answer if it wasn't asked in the question
+  # - we do this instead of validation because the code quickly becomes a
+  #   mess otherwise
+  before_save do
+    if !question.ask_subtype?
+      self.subtype = nil
+    end
+  end
   
   def self.new_with_type(params)
     Question.find(params[:question_id])&.answer(params)
@@ -16,6 +23,22 @@ class Answer < ActiveRecord::Base
   
   def available_subtypes
     Feature.subtypes_for(question.ftype)
+  end
+  
+  # Returns a score between 0 and 100 for a given area in response to this
+  # question
+  def score_area(area)
+    50
+  end
+  
+  # True if the specified area contains the feature referred to by this answer
+  def area_contains_feature?(area)
+    area.contains?(question.ftype, subtype: subtype)
+  end
+  
+  def to_s
+    subtype_str = subtype? ? " (#{subtype})" : ""
+    "<Q: #{question.text} A: #{answer}#{subtype_str}>"
   end
   
   private
@@ -30,6 +53,13 @@ end
 class ImportanceAnswer < Answer
   enum answer: [:essential, :important, :irrelevant, :bad]
   
+  # If the answer was :irrelevant or :bad then the subtype isn't used - remove it
+  before_save do
+    if irrelevant? or bad?
+      self.subtype = nil
+    end
+  end
+  
   def self.model_name
     Answer.model_name
   end
@@ -37,10 +67,31 @@ class ImportanceAnswer < Answer
   def matching_question_class
     ImportanceQuestion
   end
+  
+  def score_area(area)
+    contains = area_contains_feature?(area)
+    if essential?
+      contains ? 100 : 0
+    elsif important?
+      contains ? 75 : 25
+    elsif bad?
+      contains ? 25 : 75
+    else
+      50 # if irrelevant it doesn't matter either way
+    end
+  end
+  
 end
 
 class BooleanAnswer < Answer
   enum answer: [:yes, :no]
+  
+  # If the answer was no, remove the subtype
+  before_save do
+    if no?
+      self.subtype = nil
+    end
+  end
   
   def self.model_name
     Answer.model_name
@@ -48,5 +99,23 @@ class BooleanAnswer < Answer
   
   def matching_question_class
     BooleanQuestion 
+  end
+  
+  # If the answer is 'no' (you don't care) then subtype should be ignored
+  def subtype
+    if no?
+      nil
+    else
+      super
+    end
+  end    
+  
+  def score_area(area)
+    contains = area_contains_feature?(area)
+    if yes?
+      contains ? 100 : 0
+    else
+      contains ? 50 : 100
+    end
   end
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -27,4 +27,37 @@ RSpec.describe Answer, type: :model do
     end
   end
   
+  describe 'subtype deletion' do
+    context 'question with ask_subtype=true' do
+      context ImportanceAnswer do
+        let(:question) { create :importance_question, ask_subtype: true }
+        it 'deletes subtype for answer=irrelevant' do
+          a = create :importance_answer, question: question, user: user, answer: :irrelevant, subtype: 'foo'
+          expect(a.subtype).to be nil
+        end
+        it 'does not delete subtype for answer=essential' do
+          a = create :importance_answer, question: question, user: user, answer: :essential, subtype: 'foo'
+          expect(a.subtype).to eq('foo')
+        end
+        context 'question with ask_subtype=false' do
+          let(:question) { create :importance_question, ask_subtype: false }
+          it 'deletes subtype even if answer=essential' do
+            a = create :importance_answer, question: question, user: user, answer: :essential, subtype: 'foo'
+            expect(a.subtype).to be nil
+          end
+        end
+      end
+      context BooleanAnswer do
+        let(:question) { create :boolean_question, ask_subtype: true }
+        it 'deletes subtype for answer=no' do
+          a = create :boolean_answer, question: question, user: user, answer: :no, subtype: 'foo'
+          expect(a.subtype).to be nil
+        end
+        it 'does not delete subtype for answer=yes' do
+          a = create :boolean_answer, question: question, user: user, answer: :yes, subtype: 'foo'
+          expect(a.subtype).to eq('foo')
+        end
+      end
+    end
+  end
 end
