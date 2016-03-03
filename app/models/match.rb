8298c51fb47a63a3c249da6e370717d27197d36c
Rich Daley <rich@fishpercolator.co.uk>
2016-03-03 16:34:21 +0000

    Pretty match display page

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,6 +3,17 @@ class Match
   
   attr_accessor :user, :area, :score
   
+  def goodness
+    case score.to_i
+    when 51..100
+      :good
+    when 50
+      :middle
+    else
+      :bad
+    end
+  end
+  
   def good_answers
     user.answers.select {|a| a.score_area(area) > 50 }
   end
