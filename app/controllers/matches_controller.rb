b110aefb0e603b3d4875f29d180bfc841e4843d5
Rich Daley <rich@fishpercolator.co.uk>
2016-03-08 15:33:59 +0000

    Move the json stuff into a helper because it's view-specific

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -5,11 +5,9 @@ class MatchesController < ApplicationController
   def index
     authorize Match
     @matches = current_user.matches
-    @map_data = map_data(@matches[0..4])
   end
   
   def show
-    @map_data = map_data([@match])
   end
   
   private
@@ -17,22 +15,6 @@ class MatchesController < ApplicationController
   def set_match
     @match = current_user.matches.find {|m| m.area.name == params[:area]} || not_found
     authorize @match
-  end
-  
-  def colorgen
-    @cg ||= ColorGenerator.new saturation: 0.5, lightness: 0.5
-  end
-  
-  def map_data(matches)
-    matches.map do |m|
-      {
-        polygon: m.area.polygon,
-        name: m.area.name,
-        score: m.score.to_i,
-        path: match_path(area: m.area.name),
-        color: matches.length > 1 ? '#' + colorgen.create_hex : '#000000'
-      }
-    end
   end
     
 end
--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -5,4 +5,23 @@ module MatchesHelper
     features.map {|f| {latlng: [f.lat, f.lng], name: f.name} }.to_json
   end
   
+  # Gets all the JSON needed to render a match as a polygon on the map
+  def matches_map_json(matches)
+    matches.map do |m|
+      {
+        polygon: m.area.polygon,
+        name: m.area.name,
+        score: m.score.to_i,
+        path: match_path(area: m.area.name),
+        color: matches.length > 1 ? '#' + colorgen.create_hex : '#000000'
+      }
+    end.to_json
+  end
+  
+  private
+  
+  def colorgen
+    @cg ||= ColorGenerator.new saturation: 0.5, lightness: 0.5
+  end
+  
 end
