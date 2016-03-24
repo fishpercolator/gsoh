e239c5c81366dddafc82b1c2d8ca74a7f5ad6642
Rich Daley <rich@fishpercolator.co.uk>
2016-03-24 10:09:45 +0000

    Be responsive on questions pages

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -2,4 +2,8 @@ module AnswersHelper
   def remaining_questions
     current_user.unanswered_questions.count
   end
+  # Percentage of the way through answering the questions
+  def progress
+    (current_user.answers.count * 100) / Question.count
+  end
 end
