123849c0679a1e15677d3703cecdd25626753612
Rich Daley <rich@fishpercolator.co.uk>
2016-05-11 11:44:47 +0100

    Let's try this for size

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,8 +6,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      flash[:error] = "Error signing in with Facebook: #{@user.errors.full_messages}"
+      redirect_to new_user_session_url
     end
   end
   
@@ -18,8 +18,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
     else
-      session["devise.twitter_data"] = request.env["omniauth.auth"].except("extra")
-      redirect_to new_user_registration_url
+      flash[:error] = "Error signing in with Twitter: #{@user.errors.full_messages}"
+      redirect_to new_user_session_url
     end
   end
 
